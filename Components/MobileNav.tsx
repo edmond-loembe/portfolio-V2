da88f70bbb6560b7a4006587625a530097abb721
Loembe edmond <edmond@air-de-edmond.home>
2024-04-24 11:56:58 +0200

    [FIX] mobile Nav bar

--- Components/MobileNav.tsx
+++ Components/MobileNav.tsx
@@ -7,17 +7,24 @@ interface Props {
 }
 
 const MobileNav = ({nav, closeNav}:Props) => {
+
+    const handleScroll = (elementId: string) => {
+        const element = document.getElementById(elementId);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }
+      };
     const navAnimation = nav ? 'translate-x-0' : 'translate-x-[-100%]';
 
     return (
         <div className={`fixed ${navAnimation} transform transition-all duration-300 top-0 left-0 right-0 bottom-0 z-[1000000] bg-[#09101a]`}>
             <div className='w-[100vw] h-[100vh] flex flex-col items-center justify-center'>
-                <div className='nav-link-mobile'>HOME</div>
-                <div className='nav-link-mobile'>ABOUT</div>
-                <div className='nav-link-mobile'>SERVICES</div>
-                <div className='nav-link-mobile'>SKILLS</div>
-                <div className='nav-link-mobile'>PROJECTS</div>
-                <div className='nav-link-mobile'>CONTACT</div>
+                <div onClick={closeNav}  className='nav-link-mobile'><a onClick={() => handleScroll('hero')}>HOME</a></div>
+                <div onClick={closeNav}  className='nav-link-mobile'><a onClick={() => handleScroll('about')}>ABOUT</a></div>
+                <div onClick={closeNav}  className='nav-link-mobile'><a onClick={() => handleScroll('services')}>SERVICES</a></div>
+                <div onClick={closeNav}  className='nav-link-mobile'><a onClick={() => handleScroll('skils')}>SKILLS</a></div>
+                <div onClick={closeNav}  className='nav-link-mobile'><a onClick={() => handleScroll('projects')}>PROJECTS</a></div>
+                <div onClick={closeNav}  className='nav-link-mobile'><a onClick={() => handleScroll('footer')}>CONTACT</a></div>
             </div>
             <div onClick={closeNav} className='absolute z-[100000000] cursor-pointer top-[2rem] right-[2rem] w-[2rem] h-[2rem] text-blue-400'>
                 <XMarkIcon/>
